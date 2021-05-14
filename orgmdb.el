f08e5c9c9354c6c895d3fbf2f76f691c4edec811
Isa Mert Gurbuz <isamertgurbuz@gmail.com>
2021-05-14 22:51:35 +0300

    Add two new functions and rework `orgmdb-fill-movie-properties`
    Now `orgmdb-fill-movie-properties` tries to detect type of the current
    header (if it's a movie/series/episode etc.). When called with
    prefix-arg it also sets the headerline based on the type. It also
    tries to check if the header contains imdb-id, and searches accordingly.
    
    These functions are added:
    - orgmdb-fill-movie-properties-with-imdb-id :: Asks for imdb id to
    fill the properties.
    
    - orgmdb-fill-movie-properties-with-title :: Asks for title and year
    to fill the properties.
    
    Also fixes #2

--- orgmdb.el
+++ orgmdb.el
@@ -49,6 +49,8 @@ When `orgmdb-fill-movie-properties' is called, these properties will be
   "http://www.omdbapi.com"
   "OMDb URL.")
 
+(defconst orgmdb--types '("movie" "series" "episode"))
+
 (defun orgmdb--mk-url ()
   "Build OMDb url to do a request."
   (format  "%s/?apikey=%s" orgmdb-omdb-url orgmdb-omdb-apikey))
@@ -188,30 +190,57 @@ Some call examples:
   "Get metascore from omdb response R and default to D it does not exits."
   (orgmdb--get 'Metascore r d))
 
+(defun orgmdb--ask-for-type ()
+  "Simply ask for a type."
+  (completing-read "Type: " orgmdb--types))
+
 (defun orgmdb--ask-for-title-and-year ()
   "Simply ask for title and year from interactively."
-  `(:title ,(read-string "Title: ")
-    :year ,(read-string "Year (can be empty): ")))
-  (-if-let (header (org-entry-get nil "ITEM"))
-       (save-match-data
-         (if (string-match "\\(.+\\)(\\([0-9]\\{4\\}\\))" header)
-             `(,(s-trim (match-string 1 header)) ,(match-string 2 header))
-           `(,(s-trim header) nil)))
-     `(,(read-string "Movie name: ") ,(read-string "Movie year (can be empty): "))))
+  `(,(read-string "Title: ")
+    ,(read-string "Year (can be empty): ")))
+
+(defun orgmdb--detect-type-from-header ()
+  "Detect whether current heading is a movie or a series or an episode."
+  (interactive)
+  (let* ((type-prop (org-entry-get nil "TYPE"))
+         (type-tag (--first (member it orgmdb--types) (org-get-tags))))
+    (cond
+     (type-tag type-tag)
+     (type-prop type-prop)
+     (t (orgmdb--ask-for-type)))))
+
+(defun orgmdb--detect-params-from-header ()
+  "Get parameters for `orgmdb' function from current org header.
+If not on a org header, simpy ask from user."
+  (interactive)
+  (-let* ((header (or (org-entry-get nil "ITEM") ""))
+          (imdb-id (or (car (s-match "tt[0-9]\\{7,8\\}" header)) (org-entry-get nil "IMDB-ID")))
+          (type (when (not imdb-id)
+                  (orgmdb--detect-type-from-header)))
+          ((title year) (when (not imdb-id)
+                          (if (s-blank? header)
+                              (orgmdb--ask-for-title-and-year)
+                            (-if-let ((_ title year) (s-match "\\(.+\\)(\\([0-9]\\{4\\}\\))" header))
+                                `(,(s-trim title) ,year)
+                              `(,(s-trim (car (s-split "(" header)))))))))
+    `(:imdb ,imdb-id :type ,type :title ,title :year ,year)))
 
 ;;;###autoload
-(defun orgmdb-movie-properties (title &optional year)
-  "Open new org-buffer containing movie info and poster of given TITLE of YEAR.
-When called interactively on an org header, it will automatically detect
- TITLE and YEAR for the format \"Movie Name (YEAR)\". Otherwise it'll simply
- ask for inputs."
-  (interactive (orgmdb--detect-title-from-header))
-  (let ((info (orgmdb :title title :year year))
+(defun orgmdb-movie-properties (&rest args)
+  "Open new org-buffer containing movie info and poster of given ARGS.
+ARGS should be in the same form with `orgmdb' function.
+
+If this function is called on an org heading then it'll try to
+detect parameters based on that heading.  If not, it'll simply ask
+for title and year.  See `orgmdb-fill-movie-properties' function
+for check how parameter detection works."
+  (interactive (orgmdb--detect-params-from-header))
+  (let ((info (apply #'orgmdb args))
         (poster-file (make-temp-file "~/.cache/orgmdb_poster_" nil ".jpg")))
     (shell-command-to-string (format "curl '%s' > %s" (orgmdb-poster info) poster-file))
     (switch-to-buffer (format "*orgmdb: %s*" (orgmdb-title info)))
     (org-mode)
-    (insert (format "* %s (%s)\n" (orgmdb-title info) year))
+    (insert (format "* %s (%s)\n" (orgmdb-title info) (orgmdb-year info)))
     (insert (format "[[file:%s]]\n\n" poster-file))
     (org-display-inline-images)
     (insert (format "- Genre :: %s\n" (orgmdb-genre info)))
@@ -226,17 +255,73 @@ When called interactively on an org header, it will automatically detect
     (insert (format "- IMDb Rating :: %s\n" (orgmdb-imdb-rating info)))
     (insert (format "\n- Plot :: %s\n" (orgmdb-plot info)))))
 
+(defun orgmdb--fill-properties (info should-set-title)
+  (--map (-as-> (format "orgmdb-%s" it) fn
+                (intern fn)
+                (apply fn `(,info "N/A"))
+                (org-entry-put nil (upcase (format "%s" it)) fn))
+         orgmdb-fill-property-list)
+  (when should-set-title
+    (org-edit-headline
+     (pcase (orgmdb-type info)
+       ("movie" (format "%s (%s)" (orgmdb-title info) (orgmdb-year info)))
+       ("series" (format "%s (%s)" (orgmdb-title info) (orgmdb-year info)))
+       ("episode" (format "S%sE%s - %s" (orgmdb-season info) (orgmdb-episode info) (orgmdb-title info))))))
+  (message "Done."))
+
 ;;;###autoload
-(defun orgmdb-fill-movie-properties (title &optional year)
-  "When called on an org header, fetches and sets pre-defined properties.
-See `orgmdb-fill-property-list' for which properties will be set."
-  (interactive (orgmdb--detect-title-from-header))
-  (let ((info (orgmdb :title title :year year)))
-    (--map (-as-> (format "orgmdb-%s" it) fn
-                 (intern fn)
-                 (funcall fn info)
-                 (org-entry-put nil (upcase (format "%s" it)) fn))
-           orgmdb-fill-property-list)))
+(defun orgmdb-fill-movie-properties (should-set-title)
+  "Fetch and set pre-defined properties to current org header.
+See `orgmdb-fill-property-list' for which properties will be set.
+
+It will automatically try to automatically detect what to search
+for based on the current org header.  Following formats are
+recognized:
+
+- TITLE
+- TITLE (YEAR)
+- IMDB-ID
+
+It also checks if the header contains one of the following tags:
+\"movie\", \"series\", \"episode\" or checks if the TYPE property
+of the current header is one of these. If so, searches for that
+particular type only.
+
+If the format of the header is unrecognized, it will ask for
+title and year from user.
+
+If SHOULD-SET-TITLE is non-nil, then also set the text of current
+org header using the following formats:
+
+- For movies: TITLE (YEAR)
+- For series: TITLE (YEAR-YEAR)
+- For episodes: S01E01 - TITLE"
+  (interactive "P")
+  (orgmdb--fill-properties
+   (apply #'orgmdb (orgmdb--detect-params-from-header))
+   should-set-title))
+
+;;;###autoload
+(defun orgmdb-fill-movie-properties-with-title (should-set-title)
+  "Fetch and set pre-defined properties to current org header.
+Like `orgmdb-fill-movie-properties' but instead of automatically
+detecting what to search for, it asks for title and year."
+  (interactive "P")
+  (-let ((type (orgmdb--ask-for-type))
+         ((title year) (orgmdb--ask-for-title-and-year)))
+    (orgmdb--fill-properties
+     (orgmdb :type type :title title :year year)
+     should-set-title)))
+
+;;;###autoload
+(defun orgmdb-fill-movie-properties-with-imdb-id (should-set-title)
+  "Fetch and set pre-defined properties to current org header.
+Like `orgmdb-fill-movie-properties' but instead of automatically
+detecting what to search for, it asks for IMDb id."
+  (interactive "P")
+  (orgmdb--fill-properties
+   (orgmdb :imdb (read-string "IMDb id: "))
+   should-set-title))
 
 (provide 'orgmdb)
 ;;; orgmdb.el ends here
