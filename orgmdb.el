416b2d6e9a9db179d69798d0f034307dc9cc762d
Isa Mert Gurbuz <isamertgurbuz@gmail.com>
2025-11-05 23:27:12 +0100

    Bump version

--- orgmdb.el
+++ orgmdb.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2021 Isa Mert Gurbuz
 
 ;; Author: Isa Mert Gurbuz <isamert@protonmail.com>
-;; Version: 1.0.1
+;; Version: 1.0.2
 ;; URL: https://github.com/isamert/orgmdb.el
 ;; Package-Requires: ((emacs "27.1") (dash "2.11.0") (s "1.12.0") (org "8.0.0"))
 
